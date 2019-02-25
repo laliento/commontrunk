a48e0ab60855ed6b4d1fb230cb8c4fee150ff614
Eduardo Cruz <laliento@127.0.0.1>
2019-02-25 13:06:28 -0600

    first commit

---
+++ src/main/java/com/laliento/commontrunk/CommontrunkApplication.java
@@ -0,0 +1,13 @@
+package com.laliento.commontrunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommontrunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommontrunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laliento/commontrunk/CommontrunkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.laliento.commontrunk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommontrunkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
