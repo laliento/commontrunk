d6a47cf5bffc8351316adcf4fa589e1662f5ec77
Eduardo Cruz <laliento@127.0.0.1>
2019-02-27 16:18:36 -0600

    log4j implementation

--- src/main/java/com/laliento/commontrunk/config/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/laliento/commontrunk/config/security/CustomAccessDeniedHandler.java
@@ -6,20 +6,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
-
+	private static Logger logger = LogManager.getLogger();
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 			AccessDeniedException accessDeniedException) throws IOException, ServletException {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		if (auth != null) {
-			System.out.println(
-					"User: " + auth.getName() + " attempted to access the protected URL: " + request.getRequestURI());
+			logger.warn("User: " + auth.getName() + " attempted to access the protected URL: " + request.getRequestURI()+" Exceprion: "+ accessDeniedException.getMessage());
 		}	
 
 		response.sendRedirect(request.getContextPath() + "/accesDenied.xhtml");
